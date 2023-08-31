fc505a0d34f756d74a368956cab4e3c2bb241cfc
Karl Poulson <programmingnpc@gmail.com>
2023-08-31 16:21:05 -0600

    Refactored Model

--- stock.py
+++ stock.py
@@ -1,9 +1,9 @@
 import yfinance as yf
 import numpy as np
+import pandas as pd
+from datetime import date, timedelta
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
-import pandas_datareader.data as pdr
-from datetime import date, timedelta
 
 """
 The goal of this program is to based on features of the target stock,
@@ -14,43 +14,49 @@ My choices will be -1, -.5, 0, .5, 1 which represent the magnitude of the rise o
 # Get Apple Stock History
 tkr = yf.Ticker('AAPL')
 hist = tkr.history(period="5y")
-print(f"Columns: {hist.columns}")
+print(f"YFinance Columns Obtained from TKR.History: {hist.columns}")
 hist = hist.tz_localize(None) # fix weird date format
 
 # Get the start and end date
 end = date.today()
 start = end - timedelta(days=1825)
 
-df = hist
-
 # filter to desired columns
-df = df[['Close', 'Volume']]
-print(f"Columns used: {df.columns}")
+hist = hist[['Close', 'Volume']]
+print(f"Columns used: {hist.columns}")
+
+# Create a new dataframe that will be used in training
+features = ['priceRise', 'volumeRise']
+df = pd.DataFrame(index=hist.index, columns=features)
 
 # Take the difference between two days and apply the log function
-df['priceRise'] = np.log(df['Close'] / df['Close'].shift(1))
-df['volumeRise'] = np.log(df['Volume'] / df['Volume'].shift(1))
-df = df.dropna()
+df['priceRise'] = np.log(hist['Close'] / hist['Close'].shift(1))
+df['volumeRise'] = np.log(hist['Volume'] / hist['Volume'].shift(1))
 
 # Average out the features over the last 7 days
 df = df.resample('W').mean()
-print(f"CLOSE AND VOLUME AVERAGE 7 DAYS: \n{df}")
 
 # Refactor the df to only include engineered features
 df = df[['priceRise', 'volumeRise']]
 
 # Calculate conditions based on the price rise/fall
 conditions = [
-    (df['priceRise'].shift(-1) < -0.02), # -2 percent fall
-    (df['priceRise'].shift(-1) < -0.01), # -1 percent fall
-    (df['priceRise'].shift(-1) >= -0.01) & (df['priceRise'].shift(-1) <= 0.01), # no change
-    (df['priceRise'].shift(-1) > 0.01), # 1 percent rise
-    (df['priceRise'].shift(-1) > 0.02)  # 2 percent rise
+    (df['priceRise'].shift(-1) <= -0.01), # -2 percent fall
+    (df['priceRise'].shift(-1) <= -0.005) & (df['priceRise'].shift(-1) > -0.01), # -.5 percent fall
+    (df['priceRise'].shift(-1) >  -0.005) & (df['priceRise'].shift(-1) <  0.005), # no change
+    (df['priceRise'].shift(-1) >=  0.005) & (df['priceRise'].shift(-1) <  0.01), # .5 percent rise
+    (df['priceRise'].shift(-1) >=  0.01)  # 2 percent rise
 ]
 
 # Create a price prediction column based on percent price rise and fall
 choices = [-2, -1, 0, 1, 2]
 df['Pred'] = np.select(conditions, choices, default=0)
+
+# Display the dataframe with features and predictions
+print(f"CLOSE AND VOLUME AVERAGE 7 DAYS WITH PREDICTIONS: \n{df.head(20)}")
+
+# Display the percentage of each prediction
+print(f"Percentage of each prediction: \n{df['Pred'].value_counts(normalize=True)}")
 
 # Finalize features and 
 features = df[['priceRise', 'volumeRise']].to_numpy()
@@ -60,11 +66,14 @@ predicted_changes = df['Pred'].to_numpy()
 # Train my model
 x_train, y_train, y_train, y_test = train_test_split(features, predicted_changes, test_size=0.2)
 mlClassifier = MLPClassifier(
-    hidden_layer_sizes=(100, 100),
+    hidden_layer_sizes=(100, 100, 5),
     activation='relu',
     solver='adam',
     max_iter=1000,
     random_state=1
 )
 mlClassifier.fit(x_train, y_train)
-print(f"Score: {mlClassifier.score(x_train, y_train)}")
+print(f"Score: {mlClassifier.score(x_train, y_train)}\n\n")
+
+print("From the data posted in the past week, will the stock rise or fall?")
+print(df.tail(1))
