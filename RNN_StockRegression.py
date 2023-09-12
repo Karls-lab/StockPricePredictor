16c35d8578ebf4d552e42a60ac12e9f4e8c8a976
Karl Poulson <programmingnpc@gmail.com>
2023-09-12 12:30:57 -0600

    New commit

--- RNN_StockRegression.py
+++ RNN_StockRegression.py
@@ -3,9 +3,8 @@ import numpy as np
 import pandas as pd
 from datetime import date, timedelta
 from sklearn.model_selection import train_test_split
-from sklearn.neural_network import MLPClassifier
 import tensorflow as tf
-from tensorflow.keras.layers import Embedding, SimpleRNN, Dense
+from tensorflow.keras.layers import Dense, Conv1D
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
 
@@ -17,66 +16,72 @@ My choices will be -1, -.5, 0, .5, 1 which represent the magnitude of the rise o
 
 # Get Apple Stock History
 tkr = yf.Ticker('AAPL')
-hist = tkr.history(period="5y")
+hist = tkr.history(period="10y")
 print(f"YFinance Columns Obtained from TKR.History: {hist.columns}")
 hist = hist.tz_localize(None) # fix weird date format
 
 # Get the start and end date
-end = date.today()
-start = end - timedelta(days=1825)
+end = date(2023, 6, 1)
+start = end - timedelta(days=3650)
+
+# Filter down to weekly trends 
+hist = hist.resample('W').mean()
 
 # filter to desired columns
 hist = hist[['Close', 'Volume']]
 print(f"Columns used: {hist.columns}")
 
 # Save the dataframe to a CSV file
-hist.to_csv('savedData/stock_close_volume_rnn.csv')
+# hist.to_csv('savedData/stock_close_volume_rnn.csv')
 
 # Create a new dataframe that will be used in training
-features = ['priceRise', 'volumeRise']
-df = pd.DataFrame(index=hist.index, columns=features)
+df = pd.DataFrame(index=hist.index)
 
-# Take the difference between two days and apply the log function
-df['priceRise'] = np.log(hist['Close'] / hist['Close'].shift(1))
-df['volumeRise'] = np.log(hist['Volume'] / hist['Volume'].shift(1))
+# Take the difference between two days
+df['priceRise'] = hist['Close'].diff()
+df['volumeRise'] = hist['Volume'].diff()
 
-# Refactor the df to only include engineered features
-df = df[['priceRise', 'volumeRise']]
+# Create an average of the price rise 
+df['averagePrice'] = df['priceRise'].rolling(14).mean()
 
-# Calculate the percent change in price
-df['priceChange'] = df['priceRise'].diff().shift(-1)
+# Define the feature columns
+features_columns = ['priceRise', 'volumeRise', 'averagePrice']
+
+# Calculate the future price change of the next day
+df['futurePriceChange'] = df['priceRise'].diff(periods=-1) * -1
 
 # Remove any NaN 
 df = df.dropna()
 
 # Save the dataframe to a CSV file
-df.to_csv('savedData/stock_normalized_rnn.csv')
+# df.to_csv('savedData/stock_predictions_rnn.csv')
 
+
+"""Start the RNN Training Process"""
 # Display the dataframe with features and predictions
 print(f"LENGTH OF DF: {len(df)}")
-print(f"CLOSE AND VOLUME AVERAGE 7 DAYS WITH PREDICTIONS: \n{df.head(20)}")
+print(f"CLOSE AND VOLUME AVERAGE 7 DAYS WITH PREDICTIONS: \n{df.head(10)}")
 
-# Finalize features
-features = df[['priceRise', 'volumeRise']]
-features = np.around(features, decimals=2)
-features = features.astype('float32')
+# Round the dataframe to 3 decimal places and specify the data type
+df = np.around(df, decimals=3)
+df = df.astype('float32')
 
-# Finalize predictions
-predicted_changes = df['priceChange']
-predicted_changes = np.around(predicted_changes, decimals=3)
-predicted_changes = predicted_changes.astype('float32')
+# Split into features and predicted changes
+features = df[features_columns]
+predicted_changes = df['futurePriceChange']
 
 # Prepare for conversion to sequences
 df_combined = pd.concat([features, predicted_changes], axis=1)
 sequence_length = 10
+num_features = len(features.columns)
 X_sequences = []
 y_sequences = []
 
 # Iterate through the DataFrame to create sequences
 for i in range(len(df_combined) - sequence_length + 1):
     # Extract a sequence of length 'sequence_length'
-    sequence = df_combined[['priceRise', 'volumeRise']].iloc[i:i + sequence_length].values
-    target = df_combined['priceChange'].iloc[i + sequence_length - 1]  # Target is the prediction value at the end of the sequence
+    sequence = df_combined[features_columns].iloc[i:i + sequence_length].values
+    target = df_combined['futurePriceChange'].iloc[i + sequence_length - 1]  # Target is the prediction value at the end of the sequence
 
     X_sequences.append(sequence)
     y_sequences.append(target)
@@ -89,10 +94,11 @@ y_sequences = np.array(y_sequences)
 model = Sequential()
 model.add(LSTM(
     128, 
-    input_shape=(10, 2),
-    batch_size=10,
-    dtype='float32')) 
-model.add(Dense(10, activation='linear'))  # Softmax activation for multiclass classification
+    input_shape=(sequence_length, num_features),
+    batch_size=32,
+    dtype='float32',
+    )) 
+model.add(Dense(sequence_length, activation='linear'))  # Softmax activation for multiclass classification
 model.compile(loss='mean_squared_error', optimizer='adam')  # Use categorical crossentropy for classification
 model.summary()
 
